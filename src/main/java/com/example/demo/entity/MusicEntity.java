d20d94fa0c27970c107c04a29b1f3695fee0ea21
AnjiaLi <1185938704@qq.com>
2020-08-26 19:34:12 +0800

    新增收藏夹实体类

--- src/main/java/com/example/demo/entity/MusicEntity.java
+++ src/main/java/com/example/demo/entity/MusicEntity.java
@@ -20,60 +20,79 @@ public class MusicEntity {
     public String getmusicID() {
         return musicID;
     }
+
     public void setmusicID(String musicID) {
         this.musicID = musicID;
     }
+
     public String getmusicName() {
         return musicName;
     }
+
     public void setmusicName(String musicName) {
         this.musicName = musicName;
     }
+
     public String getmusicImage() {
         return musicImage;
     }
+
     public void setmusicImage(String musicImage) {
         this.musicImage = musicImage;
     }
+
     public String getmusicAddress() {
         return musicAddress;
     }
+
     public void setmusicAddress(String musicAddress) {
         this.musicAddress = musicAddress;
     }
+
     public String getmusiclookTime() {
         return musiclookTime;
     }
+
     public void setmusiclookTime(String musiclookTime) {
         this.musiclookTime = musiclookTime;
     }
+
     public String getSinger() {
         return singer;
     }
+
     public void setSinger(String Singer) {
         this.singer = Singer;
     }
+
     public String getmusicLeaving() {
         return musicLeaving;
     }
+
     public void setmusicLeaving(String musicLeaving) {
         this.musicLeaving = musicLeaving;
     }
+
     public String getmusicTime() {
         return musicTime;
     }
+
     public void setmusicTime(String musicTime) {
         this.musicTime = musicTime;
     }
+
     public String getmusicState() {
         return musicState;
     }
+
     public void setmusicState(String musicState) {
         this.musicState = musicState;
     }
+
     public String getmusiccAtegory() {
         return musiccAtegory;
     }
+
     public void setmusiccAtegory(String musiccAtegory) {
         this.musiccAtegory = musiccAtegory;
     }
